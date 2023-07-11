248f06658114a2041ffcc9b5ca025bbf145e82c4
Miguel <miguelmds925@gmail.com>
2023-07-11 18:25:17 -0300

    Aprendend os types primitivos

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,4 @@
-let number1 = document.getElementById("number1") as HTMLInputElement
+/*let number1 = document.getElementById("number1") as HTMLInputElement
 let number2 = document.getElementById("number2") as HTMLInputElement
 let button = document.getElementById("calculator") as HTMLButtonElement
 let res = document.getElementById("result") as HTMLDivElement
@@ -9,4 +9,9 @@ function calcular(n1: number, n2: number) {
 
 button.addEventListener("click", function() {
   res.innerHTML = calcular(+number1.value, +number2.value).toString()
-})
+})*/
+
+let nome: string = "Miguel"
+let idade = 90
+
+let programador: boolean = true
