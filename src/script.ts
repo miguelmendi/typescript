09948964fe7281744448139958ee2d59ca2004af
Miguel <miguelmds925@gmail.com>
2023-07-11 12:23:44 -0300

    Aprendendo  como coverter arquivos typescript em JavaScript

---
+++ src/script.ts
@@ -0,0 +1,12 @@
+let number1 = document.getElementById("number1") as HTMLInputElement
+let number2 = document.getElementById("number2") as HTMLInputElement
+let button = document.getElementById("calculator") as HTMLButtonElement
+let res = document.getElementById("result") as HTMLDivElement
+
+function calcular(n1: number, n2: number) {
+  return n1 + n2
+}
+
+button.addEventListener("click", function() {
+  res.innerHTML = calcular(+number1.value, +number2.value).toString()
+})
